bc2f105b67dc0edf5869baa5c21e571fc259e948
Igor Martins <dev.igor.martins@gmail.com>
2023-09-27 00:59:19 -0300

    Entidades modeladas

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -26,6 +26,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String phone;
     private LocalDate birthDate;
